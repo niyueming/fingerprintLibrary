d3a976abe8c7ed5892f46708c5754b1122926452
niyueming <niyueming@163.com>
2017-03-02 17:27:44 +0800

    完成，dialog的ui还需要优化

---
+++ src/main/java/net/nym/fingerprintlibrary/FingerPrintUtils.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2017  Ni YueMing<niyueming@163.com>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package net.nym.fingerprintlibrary;
+
+import android.Manifest;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+
+import static android.Manifest.permission.USE_FINGERPRINT;
+
+/**
+ * @author niyueming
+ * @date 2017-03-02
+ * @time 10:48
+ */
+
+public class FingerPrintUtils {
+
+    private FingerPrintUtils() {
+    }
+
+    public static boolean check(Context context) {
+        return (FingerPrintUtils.isKeyguardSecure(context)
+                && FingerPrintUtils.hasFingerprintPermission(context)
+                && FingerPrintUtils.hasEnrolledFingerprints(context)
+        );
+    }
+
+    public static boolean isKeyguardSecure(Context context) {
+        KeyguardManager keyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+        boolean isKeyguardSecure = keyguardManager.isKeyguardSecure();
+        if (!isKeyguardSecure) {
+            System.out.println("设置界面中未开启密码锁屏功能");
+        }
+        return isKeyguardSecure;
+    }
+
+    public static FingerprintManager getFingerprintManager(Context context) {
+        boolean isFlag = false;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            isFlag = true;
+        } else if (isHaveFingerprintManager()) {
+            isFlag = true;
+        }
+
+        if (isFlag) {
+            FingerprintManager fingerprintManager;
+            fingerprintManager = (FingerprintManager) context.getSystemService("fingerprint");
+            return fingerprintManager;
+        }
+        return null;
+    }
+
+    public static boolean hasEnrolledFingerprints(Context context) {
+        boolean isFlag = false;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            isFlag = true;
+        } else if (isHaveFingerprintManager()) {
+            isFlag = true;
+
+        }
+
+        if (isFlag) {
+            FingerprintManager fingerprintManager;
+            fingerprintManager = (FingerprintManager) context.getSystemService("fingerprint");
+            if (ActivityCompat.checkSelfPermission(context, USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                System.out.println("没有权限：android.permission.USE_FINGERPRINT");
+                return false;
+            }
+            boolean hasEnrolledFingerprints = fingerprintManager.hasEnrolledFingerprints();
+            if (!hasEnrolledFingerprints) {
+                System.out.println("您还没有录入指纹, 请在设置界面录入至少一个指纹");
+            }
+            return hasEnrolledFingerprints;
+        }
+        return false;
+
+    }
+
+    public static boolean hasFingerprintPermission(Context context) {
+        if (ActivityCompat.checkSelfPermission(context, USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            System.out.println("没有权限：android.permission.USE_FINGERPRINT");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isHaveFingerprintManager() {
+        try {
+            Class.forName("android.hardware.fingerprint.FingerprintManager"); // 通过反射判断是否存在该类
+            return true;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void authenticate(Context context,
+                                    @Nullable FingerprintManager.CryptoObject crypto,
+                                    @Nullable CancellationSignal cancel,
+                                    @NonNull FingerprintManager.AuthenticationCallback callback,
+                                    @Nullable Handler handler) {
+        FingerprintManager fingerprintManager = getFingerprintManager(context);
+        if (fingerprintManager == null) {
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fingerprintManager.authenticate(crypto,cancel,0,callback,handler);
+    }
+
+    public static void authenticate(Context context,
+                                    @NonNull FingerprintManager.AuthenticationCallback callback) {
+        authenticate(context,null,null,callback,null);
+    }
+
+    public static void authenticate(Context context,
+                                    @Nullable FingerprintManager.CryptoObject crypto,
+                                    @Nullable CancellationSignal cancel,
+                                    @NonNull FingerprintManager.AuthenticationCallback callback) {
+        authenticate(context,crypto,cancel,callback,null);
+    }
+}
---
+++ src/main/java/net/nym/fingerprintlibrary/dialog/FingerprintDialog.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2017  Ni YueMing<niyueming@163.com>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package net.nym.fingerprintlibrary.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import net.nym.fingerprintlibrary.FingerPrintUtils;
+import net.nym.fingerprintlibrary.R;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * @author niyueming
+ * @date 2017-03-02
+ * @time 13:48
+ */
+
+public class FingerprintDialog extends Dialog {
+    private TextView txt_title;
+    private Button btn_password;
+    private ImageView fingerprint;
+    private OnClickListener mOnClickListener;
+    private CancellationSignal mCancellationSignal;
+    private FingerprintManager.AuthenticationCallback mAuthenticationCallback;
+    private String mTitle;
+    private RevisionHandler mHandler;
+    private OnFingerPrintCallback mOnFingerPrintCallback;
+
+
+    public FingerprintDialog(Context context) {
+        super(context);
+    }
+
+    public FingerprintDialog(Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    protected FingerprintDialog(Context context, boolean cancelable, OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_fingerprint);
+        txt_title = (TextView) findViewById(R.id.title);
+        txt_title.setText(mTitle != null ? mTitle: getContext().getText(R.string.fp_unlock_title));
+        btn_password = (Button) findViewById(R.id.to_password);
+        btn_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnClickListener != null){
+                    mOnClickListener.onClick(FingerprintDialog.this,-1);
+                }
+                onBackPressed();
+            }
+        });
+        fingerprint = (ImageView) findViewById(R.id.fingerprint);
+        mTitle = txt_title.getText().toString().trim();
+        setCanceledOnTouchOutside(false);
+
+        mHandler = new RevisionHandler(this);
+        if (!FingerPrintUtils.check(getContext())){
+            exit();
+            return;
+        }
+
+        mCancellationSignal = new CancellationSignal();
+        mAuthenticationCallback = new FingerprintManager.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                System.out.println("errString=" + errString + ",errorCode=" + errorCode);
+                txt_title.setText(errString);
+                exit();
+            }
+
+            @Override
+            public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+                System.out.println("helpString=" + helpString+ ",helpCode=" + helpCode);
+                super.onAuthenticationHelp(helpCode, helpString);
+                txt_title.setText(helpString);
+                revision();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                System.out.println("onAuthenticationSucceeded=" + result.toString());
+                if (mOnFingerPrintCallback != null){
+                    mOnFingerPrintCallback.onAuthenticationSucceeded(result);
+                }
+                onBackPressed();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                System.out.println("onAuthenticationFailed");
+                txt_title.setText(R.string.fp_try_again);
+                revision();
+            }
+        };
+        FingerPrintUtils.authenticate(getContext(),null,mCancellationSignal,mAuthenticationCallback);
+
+    }
+
+    private void exit() {
+        mHandler.sendEmptyMessageDelayed(1,1000);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus){
+            AnimationDrawable animationDrawable = (AnimationDrawable) fingerprint.getDrawable();
+            animationDrawable.start();
+        }
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.mOnClickListener = onClickListener;
+    }
+
+    public void setOnFingerPrintCallback(OnFingerPrintCallback onFingerPrintCallback) {
+        this.mOnFingerPrintCallback = onFingerPrintCallback;
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = String.valueOf(title);
+    }
+
+    @Override
+    public void setTitle(int titleId) {
+        mTitle = String.valueOf(getContext().getText(titleId));
+    }
+
+    @Override
+    public void dismiss() {
+        if (mCancellationSignal != null){
+            mCancellationSignal.cancel();
+            mCancellationSignal = null;
+        }
+        super.dismiss();
+    }
+
+    @Override
+    public void show() {
+
+        super.show();
+    }
+
+    public void revision(){
+        Message msg = mHandler.obtainMessage();
+        msg.what = 0;
+        msg.obj = mTitle;
+        mHandler.sendMessageDelayed(msg,1 * 1000);
+    }
+
+    private static class RevisionHandler extends Handler {
+        WeakReference<FingerprintDialog> mDialog;
+
+        RevisionHandler(FingerprintDialog dialog) {
+            mDialog = new WeakReference<FingerprintDialog>(dialog);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            FingerprintDialog dialog = mDialog.get();
+            if (dialog == null){
+                return;
+            }
+            switch (msg.what){
+                case 0:
+                    String title = String.valueOf(msg.obj);
+                    dialog.setTitle(title);
+                    break;
+                case 1:
+                    dialog.onBackPressed();
+                    break;
+            }
+        }
+    }
+
+    static interface OnFingerPrintCallback{
+        void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result);
+    }
+
+}
